933a4f0d166c4d92e67c6edb8352d2186410ee7e
DEADBYTE <termay10000@gmail.com>
2026-06-25 19:03:15 +0700

    bind gen

--- scripts/gen-win-port.ps1
+++ scripts/gen-win-port.ps1
@@ -7,18 +7,18 @@ $NL = [string][char]10
 $DQ = [string][char]34
 function Swap([string]$a,[string]$b,[string]$n){ if(-not $script:s.Contains($a)){ throw "missing: $n" }; $script:s=$script:s.Replace($a,$b) }
 Swap ".extern CloseHandle`n" ".extern CloseHandle`n.extern GetCommandLineA`n" 'extern'
-Swap 'DEADWIRE HTTPD v0.3.0 ACCESS LOG' 'DEADWIRE HTTPD v0.2.0 PORT ARG' 'banner'
-Swap 'listening on http://127.0.0.1:18080' 'listening on http://127.0.0.1:<port>' 'port banner'
-Swap 'fatal: bind failed; is port 18080 already in use?' 'fatal: bind failed; port unavailable' 'bind text'
-Swap ('fatal_bind_end:'+$NL+'fatal_listen:') ('fatal_bind_end:'+$NL+'fatal_port:   .ascii '+$DQ+'fatal: bad port\r\n'+$DQ+$NL+'fatal_port_end:'+$NL+'fatal_listen:') 'port text'
+Swap 'DEADWIRE HTTPD v0.3.0 ACCESS LOG' 'DEADWIRE HTTPD v0.3.0 BIND ARG' 'banner'
+Swap 'listening on http://127.0.0.1:18080' 'listening on http://<bind>:<port>' 'listen line'
+Swap 'fatal: bind failed; is port 18080 already in use?' 'fatal: bind failed; address unavailable' 'bind text'
+Swap ('fatal_bind_end:'+$NL+'fatal_listen:') ('fatal_bind_end:'+$NL+'fatal_arg:    .ascii '+$DQ+'fatal: bad arg\r\n'+$DQ+$NL+'fatal_arg_end:'+$NL+'fatal_listen:') 'arg text'
 Swap ('.section .data'+$NL+'reuse_one: .long 1') 'reuse_one: .long 1' 'data mark'
 Swap ('health_path_end:'+$NL+$NL+'sockaddr_in:') ('health_path_end:'+$NL+$NL+'.section .data'+$NL+'sockaddr_in:') 'sockaddr data'
 Swap '    .word 0xa046              # port 18080 in network byte order' '    .word 0xa046              # default port 18080 in network byte order' 'port comment'
-Swap ('mainCRTStartup:'+$NL+'    push rbp'+$NL+'    mov rbp, rsp'+$NL+'    sub rsp, 64'+$NL+$NL+'    lea rcx, [rip + banner]') ('mainCRTStartup:'+$NL+'    push rbp'+$NL+'    mov rbp, rsp'+$NL+'    sub rsp, 64'+$NL+$NL+'    call configure_port'+$NL+'    jc .die_port'+$NL+$NL+'    lea rcx, [rip + banner]') 'hook'
-Swap ('.die_wsa:'+$NL+'    lea rcx, [rip + fatal_wsa]') ('.die_port:'+$NL+'    lea rcx, [rip + fatal_port]'+$NL+'    mov rdx, fatal_port_end - fatal_port'+$NL+'    call die'+$NL+'.die_wsa:'+$NL+'    lea rcx, [rip + fatal_wsa]') 'die port'
+Swap ('mainCRTStartup:'+$NL+'    push rbp'+$NL+'    mov rbp, rsp'+$NL+'    sub rsp, 64'+$NL+$NL+'    lea rcx, [rip + banner]') ('mainCRTStartup:'+$NL+'    push rbp'+$NL+'    mov rbp, rsp'+$NL+'    sub rsp, 64'+$NL+$NL+'    call configure_args'+$NL+'    jc .die_arg'+$NL+$NL+'    lea rcx, [rip + banner]') 'hook'
+Swap ('.die_wsa:'+$NL+'    lea rcx, [rip + fatal_wsa]') ('.die_arg:'+$NL+'    lea rcx, [rip + fatal_arg]'+$NL+'    mov rdx, fatal_arg_end - fatal_arg'+$NL+'    call die'+$NL+'.die_wsa:'+$NL+'    lea rcx, [rip + fatal_wsa]') 'die arg'
 $fn = @'
-# configure_port() parses optional command-line port into sockaddr_in
-configure_port:
+# configure_args() parses optional: <port> [127.0.0.1|0.0.0.0]
+configure_args:
     push rbx
     sub rsp, 32
     call GetCommandLineA
@@ -27,61 +27,64 @@ configure_port:
     mov rbx, rax
     cmp byte ptr [rbx], '"'
     je .quoted_exe
-
 .unquoted_exe:
     mov al, byte ptr [rbx]
     test al, al
-    je .no_arg
+    je .ok
     cmp al, ' '
     je .after_exe
     cmp al, 9
     je .after_exe
     inc rbx
     jmp .unquoted_exe
-
 .quoted_exe:
     inc rbx
 .quoted_loop:
     mov al, byte ptr [rbx]
     test al, al
-    je .no_arg
+    je .ok
     cmp al, '"'
     je .after_quote
     inc rbx
     jmp .quoted_loop
-
 .after_quote:
     inc rbx
-
 .after_exe:
-.skip_ws:
+    call skip_ws
+    cmp byte ptr [rbx], 0
+    je .ok
+    mov rcx, rbx
+    call parse_port_arg
+    jc .bad
+    mov word ptr [rip + sockaddr_in + 2], ax
+    mov rbx, rcx
+    call skip_ws
+    cmp byte ptr [rbx], 0
+    je .ok
+    mov rcx, rbx
+    call parse_bind_arg
+    jc .bad
+    mov dword ptr [rip + sockaddr_in + 4], eax
+.ok:
+    clc
+    pop rbx
+    ret
+.bad:
+    stc
+    pop rbx
+    ret
+
+skip_ws:
+.skip_ws_loop:
     mov al, byte ptr [rbx]
     cmp al, ' '
     je .skip_one
     cmp al, 9
     je .skip_one
-    jmp .maybe_arg
+    ret
 .skip_one:
     inc rbx
-    jmp .skip_ws
-
-.maybe_arg:
-    cmp byte ptr [rbx], 0
-    je .no_arg
-    mov rcx, rbx
-    call parse_port_arg
-    jc .bad_port
-    mov word ptr [rip + sockaddr_in + 2], ax
-
-.no_arg:
-    clc
-    pop rbx
-    ret
-
-.bad_port:
-    stc
-    pop rbx
-    ret
+    jmp .skip_ws_loop
 
 parse_port_arg:
     push rbx
@@ -122,6 +125,72 @@ parse_port_arg:
 .port_bad:
     stc
     pop rbx
+    ret
+
+parse_bind_arg:
+    cmp byte ptr [rcx+0], '1'
+    jne .try_any
+    cmp byte ptr [rcx+1], '2'
+    jne .bind_bad
+    cmp byte ptr [rcx+2], '7'
+    jne .bind_bad
+    cmp byte ptr [rcx+3], '.'
+    jne .bind_bad
+    cmp byte ptr [rcx+4], '0'
+    jne .bind_bad
+    cmp byte ptr [rcx+5], '.'
+    jne .bind_bad
+    cmp byte ptr [rcx+6], '0'
+    jne .bind_bad
+    cmp byte ptr [rcx+7], '.'
+    jne .bind_bad
+    cmp byte ptr [rcx+8], '1'
+    jne .bind_bad
+    mov rdx, rcx
+    add rdx, 9
+    call bind_end_ok
+    jc .bind_bad
+    mov eax, 0x0100007f
+    clc
+    ret
+.try_any:
+    cmp byte ptr [rcx+0], '0'
+    jne .bind_bad
+    cmp byte ptr [rcx+1], '.'
+    jne .bind_bad
+    cmp byte ptr [rcx+2], '0'
+    jne .bind_bad
+    cmp byte ptr [rcx+3], '.'
+    jne .bind_bad
+    cmp byte ptr [rcx+4], '0'
+    jne .bind_bad
+    cmp byte ptr [rcx+5], '.'
+    jne .bind_bad
+    cmp byte ptr [rcx+6], '0'
+    jne .bind_bad
+    mov rdx, rcx
+    add rdx, 7
+    call bind_end_ok
+    jc .bind_bad
+    xor eax, eax
+    clc
+    ret
+.bind_bad:
+    stc
+    ret
+
+bind_end_ok:
+    mov al, byte ptr [rdx]
+    test al, al
+    je .end_ok
+    cmp al, ' '
+    je .end_ok
+    cmp al, 9
+    je .end_ok
+    stc
+    ret
+.end_ok:
+    clc
     ret
 
 '@
