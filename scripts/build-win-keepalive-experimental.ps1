2af797c673c847282e3a60d3557759ed8372f028
DEADBYTE <termay10000@gmail.com>
2026-06-29 14:30:36 +0700

    make experimental keepalive patch anchors robust

--- scripts/build-win-keepalive-experimental.ps1
+++ scripts/build-win-keepalive-experimental.ps1
@@ -20,66 +20,37 @@ if (-not (Test-Path $Source)) {
 $text = Get-Content $Source -Raw
 $text = $text -replace "`r`n", "`n"
 
-$anchor = @"
-handle_client:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 160
-
-    call set_type_text
-"@
-
-$replacement = @"
-handle_client:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 160
-
-.request_loop:
-    call set_type_text
-"@
-
-$count = [regex]::Matches($text, [regex]::Escape($anchor)).Count
-if ($count -ne 1) {
-    throw "build-win-keepalive-experimental: expected one handle_client prologue, found $count"
+$handleAt = $text.IndexOf("handle_client:")
+if ($handleAt -lt 0) {
+    throw 'build-win-keepalive-experimental: missing handle_client label'
 }
-$text = $text.Replace($anchor, $replacement)
 
-$healthPattern = @"
-.health:
-    lea rcx, [rip + log_200_health]
-    mov rdx, log_200_health_end - log_200_health
-    call write_stdout
-    lea rax, [rip + status_200]
-    mov qword ptr [rip + response_status_ptr], rax
-    mov qword ptr [rip + response_status_len], status_200_end - status_200
-    lea rax, [rip + body_health]
-    mov qword ptr [rip + response_body_ptr], rax
-    mov qword ptr [rip + response_body_len], body_health_end - body_health
-    call send_response
-    jmp .close_client
-"@
-
-$healthReplacement = @"
-.health:
-    lea rcx, [rip + log_200_health]
-    mov rdx, log_200_health_end - log_200_health
-    call write_stdout
-    lea rax, [rip + status_200]
-    mov qword ptr [rip + response_status_ptr], rax
-    mov qword ptr [rip + response_status_len], status_200_end - status_200
-    lea rax, [rip + body_health]
-    mov qword ptr [rip + response_body_ptr], rax
-    mov qword ptr [rip + response_body_len], body_health_end - body_health
-    call send_response
-    jmp .request_loop
-"@
-
-$count = [regex]::Matches($text, [regex]::Escape($healthPattern)).Count
-if ($count -ne 1) {
-    throw "build-win-keepalive-experimental: expected one health close path, found $count"
+$setTypeNeedle = "    call set_type_text`n"
+$setTypeAt = $text.IndexOf($setTypeNeedle, $handleAt)
+if ($setTypeAt -lt 0) {
+    throw 'build-win-keepalive-experimental: missing handle_client set_type_text call'
 }
-$text = $text.Replace($healthPattern, $healthReplacement)
+
+$loopLabel = "`n.request_loop:`n"
+if ($text.IndexOf('.request_loop:', $handleAt) -ge 0) {
+    throw 'build-win-keepalive-experimental: request loop label already exists'
+}
+$text = $text.Substring(0, $setTypeAt) + $loopLabel + $text.Substring($setTypeAt)
+
+$healthAt = $text.IndexOf(".health:")
+$badRequestAt = $text.IndexOf(".bad_request:", $healthAt)
+if ($healthAt -lt 0 -or $badRequestAt -lt $healthAt) {
+    throw 'build-win-keepalive-experimental: missing health block'
+}
+
+$healthBlock = $text.Substring($healthAt, $badRequestAt - $healthAt)
+$healthNeedle = "    call send_response`n    jmp .close_client"
+$healthCount = [regex]::Matches($healthBlock, [regex]::Escape($healthNeedle)).Count
+if ($healthCount -ne 1) {
+    throw "build-win-keepalive-experimental: expected one health response close edge, found $healthCount"
+}
+$healthBlock = $healthBlock.Replace($healthNeedle, "    call send_response`n    jmp .request_loop")
+$text = $text.Substring(0, $healthAt) + $healthBlock + $text.Substring($badRequestAt)
 
 Set-Content -Path $OutAsm -Value ($text -replace "`n", "`r`n") -NoNewline -Encoding ascii
 
