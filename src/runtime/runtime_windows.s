90d6505f81385b3cc0e519187154b7f4bcea3155
DEADBYTE <termay10000@gmail.com>
2026-06-29 18:58:32 +0700

    implement V2 runtime send_all anchor

--- src/runtime/runtime_windows.s
+++ src/runtime/runtime_windows.s
@@ -5,6 +5,8 @@
 # It is not linked into the default server build yet.
 # The live implementation remains src/deadwire_windows.s.
 
+.extern send
+
 .global dw_runtime_main
 .global dw_runtime_accept_loop
 .global dw_runtime_handle_client
@@ -28,8 +30,33 @@ dw_runtime_handle_client:
 dw_runtime_send_response:
     ret
 
-# dw_runtime_send_all maps to send_all.
+# dw_runtime_send_all(socket rcx, buffer rdx, length r8) maps to send_all.
 dw_runtime_send_all:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 64
+
+    mov qword ptr [rbp - 8], rcx
+    mov qword ptr [rbp - 16], rdx
+    mov qword ptr [rbp - 24], r8
+
+.dw_runtime_send_loop:
+    cmp qword ptr [rbp - 24], 0
+    je .dw_runtime_send_done
+    mov rcx, qword ptr [rbp - 8]
+    mov rdx, qword ptr [rbp - 16]
+    mov r8, qword ptr [rbp - 24]
+    xor r9d, r9d
+    call send
+    cmp eax, 0
+    jle .dw_runtime_send_done
+    cdqe
+    add qword ptr [rbp - 16], rax
+    sub qword ptr [rbp - 24], rax
+    jmp .dw_runtime_send_loop
+
+.dw_runtime_send_done:
+    leave
     ret
 
 # dw_runtime_write_output maps to write_stdout.
