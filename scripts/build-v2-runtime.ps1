f1ed9b75a015d83d98de28151d06acf19a146b06
DEADBYTE <termay10000@gmail.com>
2026-06-30 14:23:47 +0700

    include V2 live close object in runtime build

--- scripts/build-v2-runtime.ps1
+++ scripts/build-v2-runtime.ps1
@@ -9,6 +9,7 @@ $JoinPath = Join-Path $RepoRoot 'src/runtime/runtime_join_windows.s'
 $RunPath = Join-Path $RepoRoot 'src/runtime/runtime_run_windows.s'
 $BootPath = Join-Path $RepoRoot 'src/runtime/runtime_boot_windows.s'
 $LivePath = Join-Path $RepoRoot 'src/runtime/runtime_live_windows.s'
+$LiveClosePath = Join-Path $RepoRoot 'src/runtime/runtime_live_close_windows.s'
 $AcceptPath = Join-Path $RepoRoot 'src/runtime/runtime_accept_windows.s'
 $BridgePath = Join-Path $RepoRoot 'src/runtime/runtime_bridge_windows.s'
 $TickPath = Join-Path $RepoRoot 'src/runtime/runtime_tick_windows.s'
@@ -20,6 +21,7 @@ $ExtraObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_extra.o'
 $JoinObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_join.o'
 $RunObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_run.o'
 $LiveObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_live.o'
+$LiveCloseObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_live_close.o'
 $AcceptObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_accept.o'
 $BridgeObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_bridge.o'
 $TickObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_tick.o'
@@ -28,7 +30,7 @@ $ModeObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_mode.o'
 $BootObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_boot.o'
 $ExePath = Join-Path $BuildDir 'deadwire_v2_runtime.exe'
 
-foreach ($Path in @($RuntimePath, $LaneSetPath, $ExtraPath, $JoinPath, $RunPath, $BootPath, $LivePath, $AcceptPath, $BridgePath, $TickPath, $BoundPath, $ModePath)) {
+foreach ($Path in @($RuntimePath, $LaneSetPath, $ExtraPath, $JoinPath, $RunPath, $BootPath, $LivePath, $LiveClosePath, $AcceptPath, $BridgePath, $TickPath, $BoundPath, $ModePath)) {
     if (-not (Test-Path $Path)) {
         throw "missing V2 runtime source: $Path"
     }
@@ -56,6 +58,9 @@ if ($LASTEXITCODE -ne 0) { throw "V2 run assembly failed with exit code $LASTEXI
 & as --64 -o $LiveObjectPath $LivePath
 if ($LASTEXITCODE -ne 0) { throw "V2 live assembly failed with exit code $LASTEXITCODE" }
 
+& as --64 -o $LiveCloseObjectPath $LiveClosePath
+if ($LASTEXITCODE -ne 0) { throw "V2 live close assembly failed with exit code $LASTEXITCODE" }
+
 & as --64 -o $AcceptObjectPath $AcceptPath
 if ($LASTEXITCODE -ne 0) { throw "V2 accept assembly failed with exit code $LASTEXITCODE" }
 
@@ -74,7 +79,7 @@ if ($LASTEXITCODE -ne 0) { throw "V2 mode assembly failed with exit code $LASTEX
 & as --64 -o $BootObjectPath $BootPath
 if ($LASTEXITCODE -ne 0) { throw "V2 runtime boot assembly failed with exit code $LASTEXITCODE" }
 
-& gcc -nostdlib '-Wl,-e,mainCRTStartup' '-Wl,--subsystem,console' -o $ExePath $BootObjectPath $RuntimeObjectPath $LaneSetObjectPath $ExtraObjectPath $JoinObjectPath $RunObjectPath $LiveObjectPath $AcceptObjectPath $BridgeObjectPath $TickObjectPath $BoundObjectPath $ModeObjectPath -lws2_32 -lkernel32
+& gcc -nostdlib '-Wl,-e,mainCRTStartup' '-Wl,--subsystem,console' -o $ExePath $BootObjectPath $RuntimeObjectPath $LaneSetObjectPath $ExtraObjectPath $JoinObjectPath $RunObjectPath $LiveObjectPath $LiveCloseObjectPath $AcceptObjectPath $BridgeObjectPath $TickObjectPath $BoundObjectPath $ModeObjectPath -lws2_32 -lkernel32
 if ($LASTEXITCODE -ne 0) { throw "V2 runtime link failed with exit code $LASTEXITCODE" }
 
 if (-not (Test-Path $ExePath)) {
