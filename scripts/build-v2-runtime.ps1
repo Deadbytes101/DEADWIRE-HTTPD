acc82e439ac047f6974cd706620dc8d4a7a96a31
DEADBYTE <termay10000@gmail.com>
2026-06-30 12:14:46 +0700

    include V2 accept object in runtime build

--- scripts/build-v2-runtime.ps1
+++ scripts/build-v2-runtime.ps1
@@ -9,16 +9,18 @@ $JoinPath = Join-Path $RepoRoot 'src/runtime/runtime_join_windows.s'
 $RunPath = Join-Path $RepoRoot 'src/runtime/runtime_run_windows.s'
 $BootPath = Join-Path $RepoRoot 'src/runtime/runtime_boot_windows.s'
 $LivePath = Join-Path $RepoRoot 'src/runtime/runtime_live_windows.s'
+$AcceptPath = Join-Path $RepoRoot 'src/runtime/runtime_accept_windows.s'
 $RuntimeObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime.o'
 $LaneSetObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_lanes.o'
 $ExtraObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_extra.o'
 $JoinObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_join.o'
 $RunObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_run.o'
 $LiveObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_live.o'
+$AcceptObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_accept.o'
 $BootObjectPath = Join-Path $BuildDir 'deadwire_v2_runtime_boot.o'
 $ExePath = Join-Path $BuildDir 'deadwire_v2_runtime.exe'
 
-foreach ($Path in @($RuntimePath, $LaneSetPath, $ExtraPath, $JoinPath, $RunPath, $BootPath, $LivePath)) {
+foreach ($Path in @($RuntimePath, $LaneSetPath, $ExtraPath, $JoinPath, $RunPath, $BootPath, $LivePath, $AcceptPath)) {
     if (-not (Test-Path $Path)) {
         throw "missing V2 runtime source: $Path"
     }
@@ -58,12 +60,17 @@ if ($LASTEXITCODE -ne 0) {
     throw "V2 live assembly failed with exit code $LASTEXITCODE"
 }
 
+& as --64 -o $AcceptObjectPath $AcceptPath
+if ($LASTEXITCODE -ne 0) {
+    throw "V2 accept assembly failed with exit code $LASTEXITCODE"
+}
+
 & as --64 -o $BootObjectPath $BootPath
 if ($LASTEXITCODE -ne 0) {
     throw "V2 runtime boot assembly failed with exit code $LASTEXITCODE"
 }
 
-& gcc -nostdlib '-Wl,-e,mainCRTStartup' '-Wl,--subsystem,console' -o $ExePath $BootObjectPath $RuntimeObjectPath $LaneSetObjectPath $ExtraObjectPath $JoinObjectPath $RunObjectPath $LiveObjectPath -lws2_32 -lkernel32
+& gcc -nostdlib '-Wl,-e,mainCRTStartup' '-Wl,--subsystem,console' -o $ExePath $BootObjectPath $RuntimeObjectPath $LaneSetObjectPath $ExtraObjectPath $JoinObjectPath $RunObjectPath $LiveObjectPath $AcceptObjectPath -lws2_32 -lkernel32
 if ($LASTEXITCODE -ne 0) {
     throw "V2 runtime link failed with exit code $LASTEXITCODE"
 }
